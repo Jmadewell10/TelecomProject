83086211b78ee016df2f76cdaabb1f39f8ad4a90
rtayl126 <74155680+rtayl126@users.noreply.github.com>
2021-10-21 13:16:53 -0400

    created comps and service for ui

--- Telecom-Angular/src/app/app-routing.module.ts
+++ Telecom-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'app-home', component: HomeComponent
+  },
+  {
+    path: 'app-sign-in', component: SignInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Telecom-Angular/src/app/app.module.ts
+++ Telecom-Angular/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { HomeComponent } from './Components/home/home.component';
+import { RegisterComponent } from './Components/register/register.component';
+
+import {authInterceptorProviders} from './_helpers/auth.interceptor'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
