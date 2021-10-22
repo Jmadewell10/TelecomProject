068c83fb9670990aff2f2f32140e9456e6c81c41
rtayl126 <74155680+rtayl126@users.noreply.github.com>
2021-10-22 15:39:52 -0400

    changers

--- Telecom-Angular/src/app/app.module.ts
+++ Telecom-Angular/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './Components/sign-in/sign-in.component';
 import { HomeComponent } from './Components/home/home.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { PortalComponent } from './Components/portal/portal.component';
 
-import {authInterceptorProviders} from './_helpers/auth.interceptor'
+//import {authInterceptorProviders} from './_helpers/auth.interceptor'
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    PortalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
